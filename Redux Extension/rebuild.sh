edd335d1ceb8a496125219daffe5a3bc0525b359
Legodev <legodevgit@mailbox.org>
2018-04-11 23:01:13 +0200

    FIXED: another issue with the build script

--- Redux Extension/rebuild.sh
+++ Redux Extension/rebuild.sh
@@ -2,5 +2,5 @@ rm -rvf build/* debug-build/* Binaries/*dll Binaries/*so Binaries/Debug/ Binarie
 sh build-debug-all.sh
 sh build-all.sh
 cd Binaries/
-7z a libredex-$(date +%Y%m%d%H%M).7z Binaries/Linux/ Binaries/Windows/ libredex.cfg COPYING
+7z a libredex-$(date +%Y%m%d%H%M).7z Linux/ Windows/ libredex.cfg COPYING
 cd ..
