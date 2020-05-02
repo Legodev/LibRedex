48e665e31e8d19812803ca1979686a8e7c195284
Legodev <legodevgit@mailbox.org>
2020-05-02 20:54:20 +0200

    VERSION BUMP

--- Redux Extension/Source/constants.hpp
+++ Redux Extension/Source/constants.hpp
@@ -18,7 +18,7 @@
 #ifndef SOURCE_CONSTANTS_HPP_
 #define SOURCE_CONSTANTS_HPP_
 
-#define DLLVERSIONSTRING "[1,0,5,1]"
+#define DLLVERSIONSTRING "[1,0,5,2]"
 #define CONFIG_FILE_NAME "libredex.cfg"
 #define DB_DEBUG true
 
